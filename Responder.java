e8856a03c956e9b5e1eecb89575bfc5fd3a4e9ee
Jugador <dm.evilart@gmail.com>
2018-02-07 13:01:59 +0100

    Respuestas aleatorias implementadas

--- Responder.java
+++ Responder.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +10,32 @@
  */
 public class Responder
 {
+    Random generadorAleatorio;
+    ArrayList<String> respuestas;
+    
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        generadorAleatorio = new Random();
+        respuestas = new ArrayList<>();
+        anadirRespuestas();
+    }
+    
+    /**
+     * Anade respuestas aleatorias al servicio tecnico
+     */
+    private void anadirRespuestas(){
+        respuestas.add("Nuestros monos estan trabajando en ello");
+        respuestas.add("Su respuesta sera procesada por nuestro buzon de spam");
+        respuestas.add("Todos nuestros teleoperadores estan ignorandole");
+        respuestas.add("No le entendido, por favor, aprenda a escribir");
+        respuestas.add("Pidale a su mascota que deje el teclado");
+        respuestas.add("Pruebe a consultarlo en Google");
+        respuestas.add("Muy interesante, cuenteme mas...");
+        respuestas.add("Repita por favor, no le estaba escuchando");
+        respuestas.add("Pruebe mas tarde, es la hora del cafe");
     }
 
     /**
@@ -20,6 +44,7 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int index = generadorAleatorio.nextInt(respuestas.size());
+        return respuestas.get(index);
     }
 }
