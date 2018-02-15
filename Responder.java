8a4e9cdb227c0b2cdac8523c3f41985e81a5e227
Dasins <dm.evilart@gmail.com>
2018-02-15 23:46:12 +0100

    El robot reconoce palabras clave y da respuestas especificas

--- Responder.java
+++ Responder.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * The responder class represents a response generator object.
@@ -11,40 +12,56 @@ import java.util.ArrayList;
 public class Responder
 {
     Random generadorAleatorio;
-    ArrayList<String> respuestas;
+    ArrayList<String> respuestasDefault;
+    HashMap<String,String> respuestas;
     
     /**
-     * Construct a Responder - nothing to do
+     * Construye un Responder
      */
     public Responder()
     {
         generadorAleatorio = new Random();
-        respuestas = new ArrayList<>();
+        respuestasDefault = new ArrayList<>();
+        respuestas = new HashMap<>();
         anadirRespuestas();
     }
     
     /**
-     * Anade respuestas aleatorias al servicio tecnico
+     * Anade respuestas  al servicio tecnico
      */
     private void anadirRespuestas(){
-        respuestas.add("Nuestros monos estan trabajando en ello");
-        respuestas.add("Su respuesta sera procesada por nuestro buzon de spam");
-        respuestas.add("Todos nuestros teleoperadores estan ignorandole");
-        respuestas.add("No le entendido, por favor, aprenda a escribir");
-        respuestas.add("Pidale a su mascota que deje el teclado");
-        respuestas.add("Pruebe a consultarlo en Google");
-        respuestas.add("Muy interesante, cuenteme mas...");
-        respuestas.add("Repita por favor, no le estaba escuchando");
-        respuestas.add("Pruebe mas tarde, es la hora del cafe");
+        respuestasDefault.add("Nuestros monos estan trabajando en ello");
+        respuestasDefault.add("Su respuesta sera procesada por nuestro buzon de spam");
+        respuestasDefault.add("Todos nuestros teleoperadores estan ignorandole");
+        respuestasDefault.add("No le entiendo, por favor, aprenda a escribir");
+        respuestasDefault.add("Pidale a su mascota que deje el teclado");
+        respuestasDefault.add("Pruebe a consultarlo en Google");
+        respuestasDefault.add("Muy interesante, cuenteme mas...");
+        respuestasDefault.add("Repita por favor, no le estaba escuchando");
+        respuestasDefault.add("Pruebe mas tarde, es la hora del cafe");
+        respuestas.put("lento","Si nota que su equipo funciona con lentitud, pruebe a animarlo. Eso le dara confianza");
+        respuestas.put("hardware","No encontramos ninguna anomalia en el hardware. Por favor, revise su software?");
+        respuestas.put("software","No encontramos ninguna anomalia en el software, Por favor, revise su hardware");
+        respuestas.put("precio","El precio de la reparacion se determina con una moneda: Cara el pulmon izquierdo, cruz el derecho");
+        respuestas.put("reclamacion","En estos momentos no podemos tramitar su reclamacion debido a una desgana generalizada.");
+        respuestas.put("virus","Pruebe con paracetamol y mucha agua");
+        respuestas.put("internet","while(internet == false){router.restart()}");
     }
-
+    
     /**
      * Generate a response.
-     * @return   A string that should be displayed as the response
+     * @return A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String palabra)
     {
-        int index = generadorAleatorio.nextInt(respuestas.size());
-        return respuestas.get(index);
+        String respuesta;
+        if(respuestas.containsKey(palabra)){
+            respuesta = respuestas.get(palabra);
+        }
+        else {
+            int index = generadorAleatorio.nextInt(respuestasDefault.size());
+            respuesta = respuestasDefault.get(index);
+        }
+        return respuesta;
     }
 }
